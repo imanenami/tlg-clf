814fdf29214560c29858152248922e36a81515a3
iman <iman.enami@yahoo.com>
2018-10-31 14:46:45 +0330

    correct hold-out split in ml_clf

--- ml_clf.py
+++ ml_clf.py
@@ -3,6 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import NMF
 from nlp_tools import Preprocessor
+from emojis import find_emojis
 
 # Initialize the NLP preprocessor
 pp = Preprocessor()
@@ -27,12 +28,15 @@ df_test = df.iloc[test_indices, :]
 
 # Our NLP pre-processing pipeline
 steps = ['normalize', 'clear_emojis', 'clear_punctuation']
-df['pp_text'] = df.text.map(lambda x: pp.preprocess_pipeline(str(x), steps))
+df_train['pp_text'] = df_train.text.map(lambda x: pp.preprocess_pipeline(str(x), steps))
 
-cvec = CountVectorizer(encoding='utf-8', tokenizer=pp.tokenize, max_features=2000)
-cvec.fit(df.pp_text)
+cvec_text = CountVectorizer(encoding='utf-8', tokenizer=pp.tokenize, max_features=2000)
+cvec_text.fit(df_train.pp_text)
 
-text_features = cvec.transform(df.pp_text)
+cvec_emoji = CountVectorizer(encoding='utf-8', tokenizer=find_emojis, max_features=50)
+cvec_emoji.fit(df_train.text)
+
+text_features = cvec_text.transform(df.pp_text)
 
 nmf = NMF(n_components=100)
 nmf.fit(text_features)
