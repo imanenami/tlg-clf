dc21840b4258bf74133f2254ab512929ba5d02ae
iman <iman.enami@yahoo.com>
2018-10-31 10:46:09 +0330

    correct hold-out split in ml_clf

--- ml_clf.py
+++ ml_clf.py
@@ -1,13 +1,31 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import NMF
 from nlp_tools import Preprocessor
 
+# Initialize the NLP preprocessor
 pp = Preprocessor()
 
-df = pd.read_excel('/Users/iman/tlg_data.xlsx', encoding='utf-8')
+# Load Data
+df = pd.read_excel('C:\\Users\\mjzifan\\Documents\\tlg_data.xlsx', encoding='utf-8')
 df.dropna(inplace=True)
 
+# Drop labels for which we have not enough data
+LABELS_WITH_NOT_ENOUGH_DATA = [5, 8, 9]
+for label in LABELS_WITH_NOT_ENOUGH_DATA:
+    df.drop(df[df.label == label].index, axis=0, inplace=True)
+
+df.reset_index(inplace=True)
+
+# Take away the hold-out set
+train_indices, test_indices, _, _ =\
+    train_test_split(df.index.values, df.label.values, test_size=0.2, random_state=666, stratify=df.label.values)
+
+df_train = df.iloc[train_indices, :]
+df_test = df.iloc[test_indices, :]
+
+# Our NLP pre-processing pipeline
 steps = ['normalize', 'clear_emojis', 'clear_punctuation']
 df['pp_text'] = df.text.map(lambda x: pp.preprocess_pipeline(str(x), steps))
 
