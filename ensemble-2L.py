29f8b7b499d016ebdab9a237b75c26f6966c62ec
iman <iman.enami@yahoo.com>
2018-11-05 08:37:47 +0330

    rewrite the whole pipeline in ensemble from scratch

--- ensemble-2L.py
+++ ensemble-2L.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.preprocessing import MaxAbsScaler
+from sklearn.decomposition import NMF, TruncatedSVD
+from sklearn.preprocessing import MaxAbsScaler, FunctionTransformer
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
 from scipy.sparse import hstack
 from nlp_tools import Preprocessor
 from emojis import find_emojis
@@ -12,7 +15,7 @@ from emojis import find_emojis
 pp = Preprocessor()
 
 # Load Data
-df = pd.read_excel('/Users/iman/tlg_data.xlsx', encoding='utf-8')
+df = pd.read_excel('C:\\Users\\mjzifan\\Documents\\tlg_data.xlsx', encoding='utf-8')
 df.dropna(inplace=True)
 
 # Drop labels for which we don't have enough data
@@ -23,7 +26,7 @@ for label in LABELS_WITH_NOT_ENOUGH_DATA:
 df.reset_index(inplace=True)
 
 # Take away the hold-out set
-train_indices, test_indices, _, _ =\
+train_indices, test_indices, _, _ = \
     train_test_split(df.index.values, df.label.values, test_size=0.2, random_state=666, stratify=df.label.values)
 
 df_train = df.iloc[train_indices, :]
@@ -34,33 +37,33 @@ df_train_copy = df_train.copy()
 df_train.label[df_train.label != 1] = 2
 
 # Our NLP pre-processing pipeline
-steps = ['clear_links', 'clear_emojis', 'clear_punctuation']
-df_train['pp_text'] = df_train.text.map(lambda x: pp.preprocess_pipeline(str(x), steps))
+NLP_PREPROCESSING_STEPS = ['clear_links', 'clear_emojis', 'clear_punctuation']
+preprocess_func = lambda x: pp.preprocess_pipeline(str(x), NLP_PREPROCESSING_STEPS)
 
-cvec_text = CountVectorizer(encoding='utf-8', tokenizer=pp.filtered_tokenize, stop_words=pp.stopwords_list,
-                            max_features=4000)
-cvec_text.fit(df_train.pp_text)
+text_pipeline = Pipeline(
+    [
+        ('text_preprocess', FunctionTransformer(lambda x: list(map(preprocess_func, x)), validate=False)),
+        ('text_features', TfidfVectorizer(encoding='utf-8', tokenizer=pp.filtered_tokenize,
+                                          stop_words=pp.stopwords_list, max_features=4000))
+    ])
 
-cvec_emoji = CountVectorizer(encoding='utf-8', tokenizer=find_emojis, max_features=200)
-cvec_emoji.fit(df_train.text)
+emoji_pipeline = Pipeline(
+    [
+        ('emoji_features', CountVectorizer(encoding='utf-8', tokenizer=find_emojis, max_features=200))
+    ]
+)
 
-text_features = cvec_text.transform(df_train.pp_text)
-emoji_features = cvec_emoji.transform(df_train.text)
-features = hstack([text_features, emoji_features])
-#features=text_features
+main_pipeline = Pipeline(
+    [
+        ('union', FeatureUnion(transformer_list=[('text', text_pipeline), ('emoji', emoji_pipeline)])),
+        ('scaler', MaxAbsScaler()),
+        ('k_best', SelectKBest(chi2, k=1000)),
+        ('Classifier', RandomForestClassifier(n_estimators=256, min_samples_split=5))
+    ]
+)
 
-#nmf = NMF(n_components=500)
-#nmf.fit(features)
-#features = nmf.transform(features)
-
-scaler = MaxAbsScaler()
-scaler.fit(features)
-features = scaler.transform(features)
-
+X = df_train.text
 y = pd.get_dummies(df_train.label).values
-X = SelectKBest(chi2, k=1000).fit_transform(features, y)
-#X = features
 
-clf = RandomForestClassifier(n_estimators=512, min_samples_split=3)
-scores = cross_val_score(clf, X, y, cv=5)
+scores = cross_val_score(main_pipeline, X, y, cv=5)
 print(scores, scores.mean())
